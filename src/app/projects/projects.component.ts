0c0250696a6fd1e9d8cfbb739c4cdb30bbabb647
Kheifer Fuller <Guest@Epicodus-7G.local>
2017-09-27 11:09:43 -0700

    inject project service into project list file

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import {Project} from '../project.model';
 import {Router} from '@angular/router';
 
+import {ProjectService} from '../project.service';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
+  projects: Project[];
+
+  constructor(private router: Router, private projectService: ProjectService) { }
 
 
-  constructor(private router: Router) { }
 
 
-  projects: Project[]
+  ngOnInit() {
+    this.projects = this.projectService.getProjects();
+  }
 
-    goToDetail(clickedProject: Project){
+  goToDetail(clickedProject: Project){
       this.router.navigate(['projects', clickedProject.id]);
     }
 
-  ngOnInit() {
-  }
+
 
 }
